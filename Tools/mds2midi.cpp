5c0e4d6106183162118fc01423fb3927bc4439dd
SystemFileB <wangz7890@163.com>
2025-08-26 23:27:29 +0800

    加入mds2midi工具（WIP）

--- Tools/mds2midi.cpp
+++ Tools/mds2midi.cpp
@@ -1,1 +1,28 @@
-#include "midi.h"
+#include "mds2midi_deps.h"
+#include "mdsconvert.h"
+
+
+int main(int argc, char* argv[]) 
+{
+    if (argc != 3) {
+        printf("用法: mds2midi <输入.mds> <输出.mid>\n");
+        return 1;
+    }
+
+    auto midiData = midi::MdsToMidi(argv[1]);
+    if (!midiData) {
+        printf("转换失败：文件格式错误或读取失败\n");
+        return 2;
+    }
+
+    std::ofstream outFile(argv[2], std::ios::binary);
+    if (!outFile) {
+        printf("无法创建输出文件\n");
+        return 3;
+    }
+
+    outFile.write(reinterpret_cast<char*>(midiData->data()), midiData->size());
+    printf("成功生成MIDI文件，大小：%zu字节\n", midiData->size());
+    delete midiData;
+    return 0;
+}
---
+++ Tools/mds2midi_deps.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#pragma pack(1)
